35f39fcab8138bf6c343d6e1fa1dfdcf1e383257
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2022-01-19 11:42:28 -0800

    Convert PlantDetailQrCode

--- src/PlantDetailQrCode.tsx
+++ src/PlantDetailQrCode.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { Plant } from "./types";
 
-const googleQrCodeUrl = (destination_url) =>
+const googleQrCodeUrl = (destination_url: string) =>
   `https://chart.googleapis.com/chart?chs=500x500&cht=qr&choe=UTF-8&chl=${encodeURIComponent(
     destination_url
   )}`;
 
-const plantDetailUrlFromId = (id) =>
+const plantDetailUrlFromId = (id: number) =>
   /* example expected patterns:
     - https://some.website-of-yours.app/plants/:id/detail 
     - https://some.website-of-yours.app/plants?id=:id&utm_source=qr_code
   */
-  (process.env.REACT_APP_PLANT_DETAIL_URL_PATTERN || "").replace(":id", id);
+  (process.env.REACT_APP_PLANT_DETAIL_URL_PATTERN || "") !== ""
+    ? new URL(
+        process.env.REACT_APP_PLANT_DETAIL_URL_PATTERN.replace(
+          ":id",
+          id.toString()
+        )
+      ).toString()
+    : "";
 
-export const plantDetailQrCodeFromId = (id) => {
+export const plantDetailQrCodeFromId = (id: number) => {
   let url = plantDetailUrlFromId(id);
   return {
     destination_url: url,
@@ -20,7 +28,12 @@ export const plantDetailQrCodeFromId = (id) => {
   };
 };
 
-export const PlantDetailQrCode = ({ plant, style }) => {
+interface Props {
+  plant: Plant;
+  style: React.CSSProperties;
+}
+
+export const PlantDetailQrCode = ({ plant, style }: Props) => {
   let { destination_url, image_url } = plantDetailQrCodeFromId(plant.id);
   return (
     <a href={destination_url} target="_blank" rel="noreferrer">
@@ -28,7 +41,7 @@ export const PlantDetailQrCode = ({ plant, style }) => {
         src={image_url}
         alt={"QR Code for " + plant.botanicalName}
         className="img-responsive"
-        style={Object.assign({ width: "64px" }, style || {})}
+        style={{ width: "64px", ...(style || {}) }}
       />
     </a>
   );

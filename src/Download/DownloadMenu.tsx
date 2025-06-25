329ae0e8230722a7d13e44284830e7eb256b0d73
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2025-06-25 12:15:50 -0700

    Move SimpleModal to separate component

--- src/Download/DownloadMenu.tsx
+++ src/Download/DownloadMenu.tsx
@@ -20,6 +20,7 @@ import {
 } from "../types";
 import { plantDetailQrCodeFromId } from "../Plant/PlantDetailQrCode";
 import BenchCardDocument from "../Plant/BenchCardDocument";
+import SimpleModal from "../Shared/SimpleModal";
 
 interface DownloadMenuProps {
 	data: Data;
@@ -236,41 +237,6 @@ const DownloadMenu = ({
 
 	const actionList = <DownloadActionList downloadActions={getDownloadActions(data, searchCriteria, plants)} />;
 
-	// Updated SimpleModal with Bootstrap 4 classes
-	const SimpleModal = ({ show, onHide, children }: { show: boolean, onHide: () => void, children: React.ReactNode }) => {
-		if (!show) return null;
-		return (
-			<div className="modal fade show d-block" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-				<div className="modal-dialog modal-dialog-centered">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h4 className="modal-title">Generating Zip file</h4>
-							<button
-								type="button"
-								className="close"
-								onClick={onHide}
-								aria-label="Close"
-							>
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div className="modal-body">
-							{children}
-						</div>
-						<div className="modal-footer">
-							<button
-								type="button"
-								className="btn btn-secondary"
-								onClick={onHide}
-							>
-								Cancel
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	};
 
 	const modal = <SimpleModal
 		show={showZipModal}
